fff572a3a401cd4772227eaa3131fd9336d46a78
howell.devon@gmail.com <howell.devon@gmail.com>
2017-05-10 14:11:02 -0400

    Week5 stuff

--- Week5/week5_example/server.js
+++ Week5/week5_example/server.js
@@ -1,19 +1,26 @@
+//to use Environment variables
 require("dotenv").config();
 
 var express = require("express");
-//var dotenv = require("dotenv");
 
 var app = express();
 
-//app.use(dotenv);
-
 app.get("/", (request, response)=>{
-	response.json({
-		"user":process.env.DB_USER,
-		"pass": process.env.DB_PASS
-	});
+	console.log(process.env.NODE_ENV);
+	if(process.env.NODE_ENV == "dev"){
+		response.json({
+			"user":process.env.DB_USER,
+			"pass": process.env.DB_PASS
+		});
+	}else{
+		response.json({
+			"user": "Does not exist.",
+			"password": "Does not exist."
+		});
+	}
+
 	response.end();
 });
 
-app.listen(8080);
+app.listen(process.env.PORT || 8000);
 console.log("Server is running");
