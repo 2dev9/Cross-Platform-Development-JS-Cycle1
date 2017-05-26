38942a451597a64874b4ef99983b563aac806474
howell.devon@gmail.com <howell.devon@gmail.com>
2017-05-25 20:59:56 -0400

    Added Week8 passport practice

---
+++ Week8/passport_practice/auth.js
@@ -0,0 +1,32 @@
+var LocalStrategy = require("passport-local").Strategy,//how we authenticate a person
+	bcrypt = require("bcrypt-nodejs"),
+	User = require("./user");
+
+module.exports = function(passport) {
+	passport.serializedUser(function(user, done){
+		done(null, user);
+	});
+
+	passport.deserializedUser(function(id, done){
+		done(null, id);
+	});
+
+	passport.use("local-login", new LocalStrategy({
+		usernameField: "username",
+		passwordField: "password",
+		passReqToCallback: true
+	}, function(req, username, password, done){
+		User.findOne({"username": username}, function(err, user){
+			if(err){
+				return done(err);
+			}
+			//if the wrong user or password
+			if(!user || !bcrypt.compareSync(password, user.password)){
+				return done(null, false);
+			}
+			if(user && bcrypt.compareSync(password, user.password)){
+				return done(null, user);
+			}
+		});
+	}));
+}
